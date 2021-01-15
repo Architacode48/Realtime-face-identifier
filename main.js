80be27ecd43301403cb68f1424dd04d0da77b5c9
Archita Pandey <71716706+Architacode48@users.noreply.github.com>
2021-01-15 19:39:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 function setup(){
-    
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/FyCMjwmo-/model.json", modelLoaded);
+}
+function draw(){
+image(video, 0,0,300,300);
+classifier.classify(video,gotResult);
+}
+function modelLoaded(){
+    console.log("model loaded");
 }
